9346f5e5c6f18899e1f8ff1abb4d1e89de6300e6
Mark Tse <mark.tse@neverendingqs.com>
2019-10-19 16:13:55 -0400

    app - isDateAllowed() init.

--- src/index.js
+++ src/index.js
@@ -4,22 +4,44 @@ function parsePart(part) {
   }
 
   const pairStrs = part.split(',');
+
+  if(pairStrs.some(pairStr => pairStr === '*')) {
+    return null;
+  }
+
   return pairStrs.map(pairStr => {
     const pair = pairStr.split('-');
-    if(pair.length !== 2) {
+    if(pair.length < 1 || pair.length > 2) {
       throw new Error(`Part '${part}' has invalid pair '${pairStr}'.`);
     }
 
-    const range = { start: parseInt(pair[0]), end: parseInt(pair[1]) };
-    if(isNaN(range.start)) {
-      throw new Error(`Part '${part}' has non-integer start range '${range.start}.`);
-    }
-    if(isNaN(range.end)) {
-      throw new Error(`Part '${part}' has non-integer end range '${range.end}.`);
+    const start = parseInt(pair[0]);
+    if(isNaN(start)) {
+      throw new Error(`Part '${part}' has non-integer start range '${pair[0]}'.`);
     }
 
-    return range;
+    const end = pair.length === 1
+      ? start
+      : parseInt(pair[1]);
+
+    if(isNaN(end)) {
+      throw new Error(`Part '${part}' has non-integer end range '${pair[1]}'.`);
+    }
+
+    return { start, end };
   });
+}
+
+function isWithinRange(value, ranges) {
+  if(!ranges) {
+    return true;
+  }
+
+  return ranges.some(({ start, end }) =>
+    start < end
+      ? value >= start && value <= end
+      : value <= start || value >= end
+  );
 }
 
 module.exports = class {
@@ -36,5 +58,13 @@ module.exports = class {
     this.dayOfMonth = parsePart(parts[2]);
     this.month = parsePart(parts[3]);
     this.dayOfWeek = parsePart(parts[4]);
+  }
+
+  isDateAllowed(date) {
+    return isWithinRange(date.getMinutes(), this.minute) &&
+      isWithinRange(date.getHours(), this.hour) &&
+      isWithinRange(date.getMonth() + 1, this.dayOfMonth) &&
+      isWithinRange(date.getDate(), this.month) &&
+      isWithinRange(date.getDay(), this.dayOfWeek);
   }
 }
--- test/index.js
+++ test/index.js
@@ -25,7 +25,7 @@ describe('cron-allowed-range', function() {
       );
     });
 
-    ['5', '5-6-7', '{}', 'a-b', 'a-5', '5 - 6', '3-d'].forEach(part => {
+    ['5-6-7', '{}', 'a-b', 'a-5', '5 - 6', '3-d'].forEach(part => {
       it(`throws error on invalid part '${part}'`, function() {
         assert.throws(() => new CronAllowedRange(`* * ${part} * *`));
       });
@@ -69,6 +69,69 @@ describe('cron-allowed-range', function() {
       assert.sameDeepMembers(
         cr.dayOfWeek, [{ start: 19, end: 20 }]
       );
+    });
+
+    it('supports both singleton values, ranges, and "*" in the same expression', function() {
+      const cr = new CronAllowedRange('2 3-4 5-6,7-8 9-10 *');
+      assert.sameDeepMembers(cr.minute, [{ start: 2, end: 2 }]);
+      assert.sameDeepMembers(cr.hour, [{ start: 3, end: 4 }]);
+      assert.sameDeepMembers(cr.dayOfMonth, [{ start: 5, end: 6 }, { start: 7, end: 8 }]);
+      assert.sameDeepMembers(cr.month, [{ start: 9, end: 10 }]);
+      assert.isNull(cr.dayOfWeek);
+    });
+
+    it('ignores other ranges if a part has "*"', function() {
+      const cr = new CronAllowedRange('2 3-4 5-6,7-8 9-10 *,11-12');
+      assert.sameDeepMembers(cr.minute, [{ start: 2, end: 2 }]);
+      assert.sameDeepMembers(cr.hour, [{ start: 3, end: 4 }]);
+      assert.sameDeepMembers(cr.dayOfMonth, [{ start: 5, end: 6 }, { start: 7, end: 8 }]);
+      assert.sameDeepMembers(cr.month, [{ start: 9, end: 10 }]);
+      assert.isNull(cr.dayOfWeek);
+    });
+  });
+
+  describe('isDateAllowed()', function() {
+    /* Allowed if it is:
+     * - At any minute
+     * - Between 9 AM - 5 PM
+     * - On any day of the month
+     * - Between September to June or on August
+     * - Between Monday to Friday
+     */
+    const cr = new CronAllowedRange('* 9-17 * 9-6,8 1-5');
+
+    [
+      {
+        date: new Date('December 17, 1995 09:24:00'),
+        reason: 'it is a Sunday'
+      },
+      {
+        date: new Date('December 18, 1995 08:59:59'),
+        reason: 'it is before 9 AM'
+      },
+      {
+        date: new Date('December 18, 1995 18:24:00'),
+        reason: 'it is after 5 PM'
+      },
+      {
+        date: new Date('July 18, 1995 08:24:00'),
+        reason: 'it is on July'
+      }
+    ].forEach(({ date, reason }) => {
+      it(`returns false because ${reason}`, function() {
+        const actual = cr.isDateAllowed(date);
+        assert.isFalse(actual);
+      });
+    });
+
+    [
+      new Date('December 18, 1995 09:00:00'),
+      new Date('August 18, 1995 17:00:00')
+    ].forEach(date => {
+      it(`returns true for ${date}`, function() {
+        const actual = cr.isDateAllowed(date);
+        assert.isTrue(actual);
+      });
     });
   });
 });
